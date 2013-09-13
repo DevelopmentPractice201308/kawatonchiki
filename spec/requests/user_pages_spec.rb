93f285b710b700a3b27c70cc0f1c2b5d471973a4
miyu <miyu_m-11b@stu.kbc.ac.jp>
2013-09-13 11:22:08 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,13 +12,13 @@ describe "User pages" do
   end
 
   describe "profile page" do
-  # Code to make a user variable
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+   # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
-end
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 
 describe "signup" do
 
@@ -30,6 +30,13 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+    describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -39,11 +46,19 @@ describe "signup" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
+      
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out')}
+      end
     end
   end
-
 end
