10e5154646cc4dc23272493b51fcdbbf05a0ab86
miyu <miyu_m-11b@stu.kbc.ac.jp>
2013-09-26 18:46:37 +0900

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,6 +14,7 @@ describe "Authentication" do
    describe "signin" do
     before { visit signin_path }
        
+
     describe "with invalid information" do
         before { click_button "Sign in" }
 
@@ -26,23 +27,38 @@ describe "Authentication" do
         end
     end
 
-  describe "with valid information" do
+    describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { sign_in user }
+
+        it { should have_selector('title', text: user.name) }
+
+        it { should have_link('Users',    href: users_path) }
+        it { should have_link('Profile',  href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
+        it { should have_link('Sign out', href: signout_path) }
+
+        it { should_not have_link('Sign in', href: signin_path) }
+
+        describe "followed by signout" do
+         before { click_link "Sign out" }
+         it { should have_link('Sign in') }
+       end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      let(:non_admin) { FactoryGirl.create(:user) }
 
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
-      end
+      before { sign_in non_admin }
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-   end
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }       
+      end
+    end
   end
 end
